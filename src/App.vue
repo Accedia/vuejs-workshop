012bfde3fe45a37ebe5415fe7c7b1fbab16682c3
georgi.yolovski <georgi.yolovski@gannett.com>
2019-04-25 09:44:17 +0300

    Initial structure for router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/users">Users</router-link>
-    </div>
-
-    <router-view/>
+    
   </div>
 </template>
 
@@ -17,17 +11,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  padding-top: 30px;
 }
 </style>
--- src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>This is a home page</h1>
-  </div>
-</template>
